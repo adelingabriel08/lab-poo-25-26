2c48a9ed8610c715569f5e2aeb499416505396a6
Adelin Gabriel Chis <adelin.chis@student.upt.ro>
2025-11-19 18:31:17 +0200

    adding lab8

---
+++ Grupa1.2/LAB8/LAB8/Calculator.cs
@@ -0,0 +1,24 @@
+namespace LAB8;
+
+public class Calculator<T> : ICalculator<T> where T : System.Numerics.INumber<T>
+{
+    public T Add(T a, T b)
+    {
+        return a + b;
+    }
+    
+    public T Add(T a, T b, T c)
+    {
+        return a + b + c;
+    }
+
+    public T Subtract(T a, T b)
+    {
+        return a - b;
+    }
+
+    public T Multiply(T a, T b)
+    {
+        return a * b;
+    }
+}
---
+++ Grupa1.2/LAB8/LAB8/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LAB8;
+
+ICalculator<int> calculator = new Calculator<int>();
+ICalculator<float> calculatorFloat = new Calculator<float>();
+ICalculator<double> calculatorDouble = new Calculator<double>();
+
+
+calculatorFloat.Add(1.5f, 2.5f);
+calculatorFloat.Add(1, 2, 7);
+calculatorDouble.Add(1, 2, 7);
+calculator.Add(1, 2, 7);
+Console.WriteLine("Addition: " + calculator.Add(5, 3));
